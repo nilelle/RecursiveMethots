da646912397fd652051b2e142c192067052ba351
Bora Kaya <bora.587@hotmail.com>
2022-03-07 21:14:37 +0300

    Add project files.

---
+++ RecursiveMethots/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveMethots
+{
+    internal class Program
+    {
+        static int FibonacciSerisi(int sayi)
+        {
+            if (sayi == 0)
+                return 0;
+            else if(sayi == 1)
+        return 1;
+    else
+                return FibonacciSerisi(sayi - 1) + FibonacciSerisi(sayi - 2);
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ RecursiveMethots/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RecursiveMethots")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RecursiveMethots")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07fdece5-fbe5-4512-acbb-3ed955160de7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
